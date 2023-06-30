51ee426835800d12b9b6e53cbc2c11a2a39c72a0
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-06-30 12:47:42 +0100

    findings

---
+++ stacks/route53/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_1" {
+  description = "AWS pUblic hosted zone 1"
+  type        = string
+}
+
+variable "domain_2" {
+  description = "AWS pUblic hosted zone 2"
+  type        = string
+}
